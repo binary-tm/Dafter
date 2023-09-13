934f768b6102a930df662d72ef064a4788b9a597
‪Ahmed Shhata‬‏ <93527078+leadarbut2022@users.noreply.github.com>
2023-09-13 13:47:30 +0300

    add

---
+++ storage/framework/views/ceb0b7a312e6dbb11c999bfdf8ec02a7.php
@@ -0,0 +1,2 @@
+[<?php echo e($slot); ?>](<?php echo e($url); ?>)
+<?php /**PATH /var/www/html/pro/pro/kasat/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
